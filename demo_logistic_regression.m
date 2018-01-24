3dcc8d9e68b8a2caa12380c5687527ec9aeb8122
Chen Zu <chen0zu@gmail.com>
2018-01-24 15:00:03 +1100

    logistic regression

--- demo_logistic_regression.m
+++ demo_logistic_regression.m
@@ -1,2 +1,26 @@
 clc;
 clear;
+load iris_dataset.mat
+class1 = irisTargets(1,:) ~= 0;
+class2 = irisTargets(2,:) ~= 0;
+X = irisInputs(:,class1 | class2)';
+label = [ones(50,1);zeros(50,1)];
+
+%%
+iter = 100;
+J = zeros(iter,1);
+rng(1);
+w = rand(5,1);
+X = [ones(100,1),X];
+eta = 0.001;
+y = label;
+
+for i = 1:iter
+    hx = 1 - 1./(1+exp(X*w));
+    J(i) = sum(y.*(X*w) - log(1+exp(X*w)));
+    gw = sum(bsxfun(@times,(y - hx),X))';
+    w = w + eta * gw;
+end
+
+%%
+plot(J);
