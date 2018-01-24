96281c78559959854e6ccbca573f5824b0ea9bf8
Chen Zu <chen0zu@gmail.com>
2018-01-24 22:10:22 +1100

    SVM

--- demo_SVM.m
+++ demo_SVM.m
@@ -2,34 +2,54 @@ clc;
 clear;
 close all;
 rng(1);
-mu1 = [1,1];
-mu2 = [2,2];
-sigma = 0.05 * [1,1];
-n = 50;
-X1 = mvnrnd(mu1, sigma, n);
-X2 = mvnrnd(mu2, sigma, n);
-X = [X1;X2];
-y = [ones(n,1);-ones(n,1)];
-hold on;
-plot(X1(:,1), X1(:,2),'rx');
-plot(X2(:,1),X2(:,2),'ko');
-hold off;
-box on;
+method = 'separate';
+method = 'linear';
+method = 'kernel';
 
-%% linear separate SVM
-[n,d] = size(X);
-H = diag([ones(d,1);0]);
-A = -y.*[X,ones(n,1)];
-b = -ones(n,1);
-w = quadprog(H,[],A,b);
-b = w(end);
-w = w(1:end-1);
-
-%%
-t = 0.5:0.1:2.5;
-k = y.*(X*w+b);
-[~,idx] = sort(k);
-hold on;
-plot(t,(-b-w(1)*t)/w(2));
-plot(X(idx(1:4),1),X(idx(1:4),2),'s','markersize',10);
-hold off;
+switch method
+    case 'separate'
+        mu1 = [1,1];
+        mu2 = [2,2];
+        sigma = 0.05 * [1,1];
+        n = 50;
+        X1 = mvnrnd(mu1, sigma, n);
+        X2 = mvnrnd(mu2, sigma, n);
+        X = [X1;X2];
+        y = [ones(n,1);-ones(n,1)];
+        hold on;
+        plot(X1(:,1), X1(:,2),'rx');
+        plot(X2(:,1),X2(:,2),'ko');
+        hold off;
+        box on;
+        
+        %% linear separate SVM
+        [n,d] = size(X);
+        H = diag([ones(d,1);0]);
+        A = -y.*[X,ones(n,1)];
+        b = -ones(n,1);
+        w = quadprog(H,[],A,b);
+        b = w(end);
+        w = w(1:end-1);
+        
+        %% dual separate SVM
+        H = (X*X') .* (y*y');
+        f = -ones(n,1);
+        Aeq = y';
+        beq = 0;
+        lb = zeros(n,1);
+        alpha = quadprog(H,f,[],[],Aeq,beq,lb,[]);
+        [~,idx_dual] = sort(alpha,'descend');
+        w_dual = X' * (alpha.*y);
+        b_dual = y(idx_dual(1)) - X(idx_dual(1),:)*w_dual;
+        
+        %%
+        t = 0.5:0.1:2.5;
+        k = y.*(X*w+b);
+        [~,idx] = sort(k);
+        hold on;
+        plot(t,(-b-w(1)*t)/w(2));
+        plot(X(idx(1:4),1),X(idx(1:4),2),'s','markersize',10);
+        hold off;
+        
+    case 'linear'
+        %% linear SVM
