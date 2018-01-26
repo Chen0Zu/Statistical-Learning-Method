a5e8d1e2000b3e38acc8737f23d61524065f4c25
Chen Zu <chen0zu@gmail.com>
2018-01-26 16:05:49 +1100

    EM

--- demo_em.m
+++ demo_em.m
@@ -1,2 +1,17 @@
 clc;
 clear;
+%% Initialization
+pi = 0.4;
+p = 0.6;
+q = 0.7;
+y = [1 1 0 1 0 0 1 0 1 1];
+
+%%
+Iter = 100;
+for i = 1:Iter
+    mu = pi*(p.^y).*((1-p).^(1-y))./...
+        (pi*(p.^y).*((1-p).^(1-y))+(1-pi)*(q.^y).*(1-q).^(1-y));
+    pi = mean(mu);
+    p = mu*y'/sum(mu);
+    q = (1-mu)*y'/sum(1-mu);
+end
